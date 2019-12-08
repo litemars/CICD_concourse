c403338fa3e3fae06e257c5c23c29c088e6492b4
litemars <maxmassi12@gmail.com>
2019-12-08 16:05:12 +0100

    prova

--- script/start.sh
+++ script/start.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-echo "Hello!"
-echo "This is running from a script"
-echo "Printing 1 to 10..."
+echo "Hello-TD!"
+#echo "This is running from a script"
+#echo "Printing 1 to 10..."
 
-for x in $(seq 1 10);
-do
-  echo "$x"
-done
+#for x in $(seq 1 10);
+#do
+ # echo "$x"
+#done
 
-echo "Finished printing!"
+#echo "Finished printing!"
 ls
 cd repo
 ls
 cd script
 ls
-java test.java ${u}
+java -jar TD-test.java ${u}
--- script/test.java
+++
@@ -1,7 +0,0 @@
-public class Gatto{
-
-	public static void main(String arg[]){
-		System.out.println("ciao "+arg[0]);
-		System.out.println("come stai?");
-	}
-}
