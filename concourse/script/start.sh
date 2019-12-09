50c30e8fa26d4e89f7597e4abb51b067b34c4ddf
litemars <maxmassi12@gmail.com>
2019-12-09 14:27:48 +0100

    prova

--- concourse/script/start.sh
+++ concourse/script/start.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 
 echo "Hello-TD!"
-#echo "This is running from a script"
-#echo "Printing 1 to 10..."
-
-#for x in $(seq 1 10);
-#do
- # echo "$x"
-#done
-
-#echo "Finished printing!"
 
 pwd
-cd ./repo/concourse/script
+cd ./repo/test
 pwd
 
 
