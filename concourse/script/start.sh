a6f417a3551c322fbb03589af6cb8d6941f52e65
litemars <44295342+litemars@users.noreply.github.com>
2019-12-09 18:37:52 +0100

    file

--- concourse/script/start.sh
+++ concourse/script/start.sh
@@ -3,27 +3,6 @@
 echo "Hello-TD!"
 
 pwd
-cd repo/test
-pwd
-echo "entro in task"
-cd Task1
-echo "dopo task"
-pwd
-ls
-echo "entro in bin"
-cd bin
-echo "dopo bin"
-pwd
-echo "elenco bin"
-ls
-echo "fine elenco bin"
-cd org
-ls
-cd magee
-ls
-cd math
-ls
-cd ../../../../..
-
+cd repo/concourse/script
 
 java -jar TD-test.jar paths.txt
